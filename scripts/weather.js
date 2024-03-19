2b8383f022b076e010e70b4eef964e789b871ce3
Abubak'r Mufutau-Oseni <85508703+abubakrmo@users.noreply.github.com>
2024-03-19 21:53:11 +0000

    Removed commented code
    Commented code does the same thing as the OOP employed

--- scripts/weather.js
+++ scripts/weather.js
@@ -27,38 +27,5 @@ class Weather {
     }
 }
 
-// const key = 'tguDCD8SJmPfMjm7geAS0at0HGkR8dAd';
-
-// const getCity = async (city) =>{
-//     const resourceUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-//     const query = `?apikey=${key}&q=${city}`
-
-//     const response = await fetch(resourceUrl + query);
-//     const data = await response.json();
-
-//     return data[0];
-
-// }
-
-// const getWeather = async (locationKey) =>{
-//     const resourceUrl = 'http://dataservice.accuweather.com/currentconditions/v1/'
-//     const query = `${locationKey}?apikey=${key}`
-
-//     const response = await fetch(resourceUrl + query);
-//     const data = await response.json()
-
-//     return data;
-// }
-
-// getCity('lagos')`
-//     .then(data => {
-//         return getWeather(data.Key)
-//     })
-//     .then(data =>{
-//         // console.log(data[0])
-//     })
-//     .catch(err => console.log(err.message))
-
-
 
 
